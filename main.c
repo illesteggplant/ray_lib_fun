13536327653a18a78e8b63e4b65d0bd1aa5ddb64
illesteggplant <samwilliamson317@gmail.com>
2024-06-16 19:06:47 -0400

    testing

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main()
+int main(void)
 {
-    printf("Hello, World");
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Simple Raylib Example");
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();        // Close window and OpenGL context
+
     return 0;
 }
