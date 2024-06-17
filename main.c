55af3772b8372834bb2ddacf8cb7277e6cc7d57b
illesteggplant <samwilliamson317@gmail.com>
2024-06-17 18:14:21 -0400

    semi working Snake game (:

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 #define width 800
 #define height 600
+#define MAX_SNAKE_LENGTH 100
+
+typedef struct SnakeSegment 
+{
+    Vector2 snake_position;
+} SnakeSegment;
 
 int main(void)
 {
@@ -9,12 +15,15 @@ int main(void)
     const int screen_width = width;
     const int screen_height = height;
     int snake_size = 30;  // Size of the snake's head and the food
-    int food_size = 20;
+    int food_size = 30;
+    int score_counter = 0;
 
     InitWindow(screen_width, screen_height, "Snake. Written by Samuel");
 
     // Snake variables
-    Vector2 snake_position = { screen_width / 2, screen_height / 2 };
+    SnakeSegment snake[MAX_SNAKE_LENGTH] = { 0 };
+    int snake_length = 1;  // Initial length of the snake
+    snake[0].snake_position = (Vector2){ screen_width / 2, screen_height / 2 };
     
     // Food variables
     Vector2 food_position;
@@ -26,30 +35,61 @@ int main(void)
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        // Move the snake's body
+        for (int i = snake_length - 1; i > 0; i--)
+        {
+            snake[i].snake_position = snake[i - 1].snake_position;
+        }
+
         // For example purposes, let's move the snake with arrow keys
-        if (IsKeyDown(KEY_RIGHT)) snake_position.x += 10;
-        if (IsKeyDown(KEY_LEFT)) snake_position.x -= 10;
-        if (IsKeyDown(KEY_UP)) snake_position.y -= 10;
-        if (IsKeyDown(KEY_DOWN)) snake_position.y += 10;
+        if (IsKeyDown(KEY_RIGHT)) snake[0].snake_position.x += 12.5;
+        if (IsKeyDown(KEY_LEFT)) snake[0].snake_position.x -= 12.5;
+        if (IsKeyDown(KEY_UP)) snake[0].snake_position.y -= 12.5;
+        if (IsKeyDown(KEY_DOWN)) snake[0].snake_position.y += 12.5;
+
+        // Screen wrapping
+        if (snake[0].snake_position.x >= screen_width) 
+        {
+            snake[0].snake_position.x = 0;
+        }
+        else if (snake[0].snake_position.x < 0) 
+        {
+            snake[0].snake_position.x = screen_width - food_size;
+        }
+        if (snake[0].snake_position.y >= screen_height) 
+        {
+            snake[0].snake_position.y = 0;
+        }
+        else if (snake[0].snake_position.y < 0) 
+        {
+            snake[0].snake_position.y = screen_height - food_size;
+        }
 
         // Check for collision with food
-        if ((snake_position.x == food_position.x) && (snake_position.y == food_position.y))
+        if (CheckCollisionRecs(
+           (Rectangle){snake[0].snake_position.x, snake[0].snake_position.y, food_size, food_size},
+           (Rectangle){food_position.x, food_position.y, food_size, food_size}))
         {
             // Move food to a new random position
             food_position.x = GetRandomValue(0, (screen_width / food_size) - 1) * food_size;
             food_position.y = GetRandomValue(0, (screen_height / food_size) - 1) * food_size;
-            snake_size = snake_size + 10;
+            if (snake_length < MAX_SNAKE_LENGTH) snake_length++;
+            score_counter++;
         }
 
         BeginDrawing();
 
         ClearBackground(BLACK);
 
-        // Draw the snake's head
-        DrawRectangleV(snake_position, (Vector2){snake_size, snake_size}, GREEN);
+        for (int i = 0; i < snake_length; i++)
+        {
+            DrawRectangleV(snake[i].snake_position, (Vector2){snake_size, snake_size}, GREEN);
+        }
 
         // Draw the food
         DrawRectangleV(food_position, (Vector2){food_size, food_size}, RED);
+
+        DrawText(TextFormat("Score: %d", score_counter), 10, 10, 30, WHITE);
 
         EndDrawing();
     }
